60eabe762dc7745c95b7681c5fac27ba436a133a
chota-bheem-codes <mrwt005@gmail.com>
2024-06-14 09:32:24 +0530

    update: 5ire chain multicaller testnet

--- src/provider.ts
+++ src/provider.ts
@@ -1,7 +1,7 @@
-import { ethers } from 'ethers';
-import { all } from './call';
-import { getEthBalance } from './calls';
-import { ContractCall } from './types';
+import { ethers } from "ethers";
+import { all } from "./call";
+import { getEthBalance } from "./calls";
+import { ContractCall } from "./types";
 
 export class Provider {
   private _provider: any;
@@ -19,14 +19,14 @@ export class Provider {
 
   public getEthBalance(address: string) {
     if (!this._provider) {
-      throw new Error('Provider should be initialized before use.');
+      throw new Error("Provider should be initialized before use.");
     }
     return getEthBalance(address, this._multicallAddress);
   }
 
   public async all<T extends any[] = any[]>(calls: ContractCall[]) {
     if (!this._provider) {
-      throw new Error('Provider should be initialized before use.');
+      throw new Error("Provider should be initialized before use.");
     }
     return all<T>(calls, this._multicallAddress, this._provider);
   }
@@ -95,6 +95,7 @@ const multicallAddresses = {
   813: "0xcA11bde05977b3631167028862bE2a173976CA11",
   820: "0xcA11bde05977b3631167028862bE2a173976CA11",
   943: "0xcA11bde05977b3631167028862bE2a173976CA11",
+  997: "0x190dBB3C63a609f0fD4C08a6340f51eB913f37B2",
   999: "0xcA11bde05977b3631167028862bE2a173976CA11",
   1001: "0xcA11bde05977b3631167028862bE2a173976CA11",
   1088: "0xcA11bde05977b3631167028862bE2a173976CA11",
@@ -222,7 +223,7 @@ const multicallAddresses = {
   7545: "0xCf44A5BCD173039c37a9880cFE40Ba5691859e96",
   8545: "0x98Ef834a3415f8651A99a87929b6244D0c81A8d2",
   2494104990: "0xc18fea06c3f88359dab1a2f708b1298d5ac5caa3",
-}
+};
 
 export function setMulticallAddress(chainId: number, address: string) {
   multicallAddresses[chainId] = address;
